81e34a27e0c58f853ac640aff87181564ceaedc2
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-07-16 20:18:29 -0400

    update navigation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,19 @@
 CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/aws'                        # required
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                          # required
     aws_access_key_id:     ENV["aws_access_key_id"],       # required
